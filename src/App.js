308cf07befb9a62c012dd2fe18c7fd418024717e
Kamal Nanda <kamalnanda45@gmail.com>
2020-03-09 03:10:14 +0530

    add Card option in list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import {NavBar} from './components/NavBar/NavBar'
 import {Button} from './components/Button/Button'
 import {GridComponent} from './components/grid/GridComponent'
+import {Cards} from './components/cards/Cards';
 function App() {
   return (
     <Router>
@@ -11,11 +12,13 @@ function App() {
           <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
           <li><Link to="/button" style={{color: 'black'}} >Button</Link></li>
           <li><Link to="/grid" style={{color: 'black'}} >Grid</Link></li>
+          <li><Link to="/card" style={{color: 'black'}}>Card</Link></li>
         </ul>
       }}/>
       <Route path="/navbar" exact component={NavBar} />
       <Route path="/button" exact component={Button} />
       <Route path="/grid" exact component={GridComponent} />
+      <Route path="/card" exact component={Cards} />
     </Router>
   );
 }
