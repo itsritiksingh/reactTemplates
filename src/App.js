58f48b42f433a71a15375b3a652948781bc0a4c5
hritikg950 <hritikg950@gmail.com>
2020-03-08 12:34:40 +0530

    hgfh

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
-import {NavBar} from './components/NavBar/NavBar'
-function App() {
-  return (
-    <Router>
-      <Route path="/" exact render={()=>{
-       return <ul>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-          <li><Link to="/navbar" style={{color: 'black'}} >Navbar</Link></li>
-        </ul>
-      }}/>
-      <Route path="/navbar" exact component={NavBar} />
-    </Router>
-  );
-}
-
-export default App
-=======
 import React from 'react';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import {NavBar} from './components/NavBar/NavBar'
@@ -40,4 +15,3 @@ function App() {
 }
 
 export default App
->>>>>>> a0e9f0adf743c762eec608959f29504d0eb333c6
