1e753ef424d07166c713745d941cd09628807411
hritikg950 <hritikg950@gmail.com>
2020-03-08 12:37:08 +0530

    button

--- src/components/layouts/BasicButton/BasicButton.js
+++ src/components/layouts/BasicButton/BasicButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export function BasicButton({value,type}) {
     return (
         <>
-            <button className={`basic ${type}`}>{value}</button>      
+            <button className={`basi ${type}`}>{value}</button>      
         </>
     )
 }
