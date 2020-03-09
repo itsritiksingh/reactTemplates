cf7c625ada665a3921a6f540c451a5be3e3de161
hritikg950 <hritikg950@gmail.com>
2020-03-09 13:36:48 +0530

    input with propType

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import InputComponent from '../layouts/InputComponent/InputComponent'
+
+export function Input() {
+    return (
+        <>
+          <InputComponent type={"mdmadkm"} />  
+        </>
+    )
+}
