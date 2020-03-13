48c077eed5920840d5fd97c78e1fae63ab61b2c1
ritik singh <mr.ritik99@gmail.com>
2020-03-13 17:07:51 +0530

    app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css'
 import { NavBar } from "./components/NavBar/NavBar";
 import { Button } from "./components/Button/Button";
 import { GridComponent } from "./components/grid/GridComponent";
--- src/components/layouts/Typography/Typography.jsx
+++ src/components/layouts/Typography/Typography.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "./Typography.css";
 export const Typography = ({ style, size, className, children }) => {
-  return <div className="Typography">{}</div>;
+  return <div className="Typography" style={{...style}} classNam={className}>{children}</div>;
 };
